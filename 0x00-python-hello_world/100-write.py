a90a48628073815b11182daaa419424b363a2f32
Orcha02 <orcha2002@gmail.com>
2021-04-26 22:28:15 +0000

    Fixed checker

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys #sis = module
-#write is used to write in the standard error
+#write is used to write in the standard error, to use stderr sys is needed
 sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 exit(1)
-#to use standard error sys is needed
