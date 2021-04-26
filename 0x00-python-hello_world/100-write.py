47ccdfd3bb5ea16d1c70ed83ad0f6bafdbfff371
Orcha02 <orcha2002@gmail.com>
2021-04-26 22:19:46 +0000

    Fixed echo 0

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys #sis = module
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 #write is used to write in the standard error
-#to use standard error sys is needed
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 exit(1)
+#to use standard error sys is needed
