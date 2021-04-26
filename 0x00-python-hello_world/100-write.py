2099346f9cde395df3583db526b029f10ab5763c
Orcha02 <orcha2002@gmail.com>
2021-04-26 22:36:54 +0000

    add100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys #sis = module
-#write is used to write in the standard error, to use stderr sys is needed
+import sys  # sys is a module
+# write writes in the std error. To use stdrerr you need sys
 sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 exit(1)
