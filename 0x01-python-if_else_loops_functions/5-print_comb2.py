e08b035f3cc5a85766a6b16912cdc60922a9e666
Orcha02 <orcha2002@gmail.com>
2021-04-27 21:23:49 +0000

    Reorganize code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{0:02d}, ".format(i), end="")
-if i == 99:
-    print("{0:02d}".format(i))
+    if i == 99:
+        print("{0:02d}".format(i))
+    else:
+        print("{0:02d}".format(i), end=", ")
 # {0:02d} 02 makes reference to the width of the number
 # To show a 0 before in numbers < 10
