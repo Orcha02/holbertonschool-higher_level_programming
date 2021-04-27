a606c79535b8ae1fca36617f5ea53eec109c7a7d
Orcha02 <orcha2002@gmail.com>
2021-04-27 23:57:37 +0000

    Remove comma (,) after 89

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for num1 in range(10):
     for num2 in range(10):
-        if (num1 < num2):
-            print("{0:d}{1:d}, ".format(num1, num2), end="")
+        if num1 == 8 and num2 == 9:
+            print("{0:d}{1:d}".format(num1, num2), end="")
+        elif (num1 < num2):
+            print("{0:d}{1:d}".format(num1, num2), end=", ")
