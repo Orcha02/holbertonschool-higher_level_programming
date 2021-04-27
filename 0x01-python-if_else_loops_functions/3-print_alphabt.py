61bb60aa5c09e694761b1192e1f174efbc913e4a
Orcha02 <orcha2002@gmail.com>
2021-04-27 20:36:53 +0000

    Add +1 for 'z' to appear in output

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')):
+for i in range(ord('a'), ord('z') + 1):
+    # +1-> Para q salga la 'z'
     if i == ord('q') or i == ord('e'):
         continue
     print("{:c}".format(i), end="")
