b579d12e64a8554b2b83f344fa4312f4c0c0ca1d
Orcha02 <orcha2002@gmail.com>
2021-04-28 00:01:59 +0000

    Add \n

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num1 in range(10):
     for num2 in range(10):
         if num1 == 8 and num2 == 9:
-            print("{0:d}{1:d}".format(num1, num2), end="")
+            print("{0:d}{1:d}\n".format(num1, num2), end="")
         elif (num1 < num2):
             print("{0:d}{1:d}".format(num1, num2), end=", ")
