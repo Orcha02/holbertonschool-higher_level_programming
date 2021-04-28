100a1f9c902fe9c1faec573e0139ca5191e4b207
Orcha02 <orcha2002@gmail.com>
2021-04-28 21:10:59 +0000

    Correct output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) is 1:
+    if len(sys.argv) is 2:
         print("{:d} argument:".format(len(sys.argv) - 1))
-    elif len(sys.argv) is 0:
+    elif len(sys.argv) is 1:
         print("{:d} arguments.".format(len(sys.argv) - 1))
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {}".format(i, sys.argv[i]))
 # -1 so it doesnt count the "arguments:" to the sum of the real arguments
+# 2 instead of 1 and 1 instead of 0 to correct output due to -1
