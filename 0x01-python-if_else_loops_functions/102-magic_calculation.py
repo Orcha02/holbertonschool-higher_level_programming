9d860cf35c9665ae19fddef81b15e246d2aef375
Orcha02 <orcha2002@gmail.com>
2021-04-30 01:02:47 +0000

    Add 102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Comments allows to execute it
+# import dis
+def magic_calculation(a, b):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return ((a * b) - c)
+# dis.dis(magic_calculation)
