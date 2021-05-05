7a30fd641d719300a1a9360362f3f921d2805048
Orcha02 <orcha2002@gmail.com>
2021-05-05 22:12:28 +0000

    Add parenthesis

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
+  
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
+        return (None)
+    elif idx >= len(my_list):
+        return (None)
     else:
-        return my_list[idx]
+        return (my_list[idx])
 # [idx]-> To correct output "Element at index 3 is [1, 2, 3, 4, 5]"
