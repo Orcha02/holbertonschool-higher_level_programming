ab47e33e8eb60b3fd168ab21a59e6834cdad5050
Orcha02 <orcha2002@gmail.com>
2021-05-06 22:48:33 +0000

    Add .get to see if checker likes it

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,8 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        biggest_value_key = max(a_dictionary)
+        biggest_value_key = max(a_dictionary, key=a_dictionary.get)
         return biggest_value_key
+
+# max-> Returns the item with the highest value
+# get-> Returns value of the item with the specified key (biggest_value_key)
