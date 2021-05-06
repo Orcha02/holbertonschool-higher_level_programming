2866bcfe937fdeed71a774bda6ca98a45d014d5e
Orcha02 <orcha2002@gmail.com>
2021-05-06 23:28:44 +0000

    Add 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,5 +2,5 @@
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda value: value * number, my_list))
 
-# map() function executes a specified function for each item in an iterable
+# map() function executes a specified function for each item in an iterable.
 # (lambda argument : expression)->expression is executed and result is returned
