62a552463a6daa342cd98d963fea22122f7f7b22
Orcha02 <orcha2002@gmail.com>
2021-05-06 21:17:38 +0000

    Add 3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
+
+# intersection-> Returns the common element between the sets
+# Also works with (return set_1 & set_2)
