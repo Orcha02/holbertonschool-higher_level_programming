ec6c52a888b4be76ff7462a0913da6d828ed13b1
Orcha02 <orcha2002@gmail.com>
2021-05-17 19:55:38 +0000

    Add [0:x]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
-        for elements in my_list:
+        for elements in my_list[0:x]:
             print("{}".format(elements), end="")
             counter += 1
     except IndexError:
-        print("Invalid Index")
+        print("INVALID INDEX")
     finally:
         print()
         return counter
-
-# finally-> Will print if there's an error or not
