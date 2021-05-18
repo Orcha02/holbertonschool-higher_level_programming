7f2830171971d4885fa377e996f52ecf9fd5d968
Orcha02 <orcha2002@gmail.com>
2021-05-18 21:33:51 +0000

    implement more comments

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,10 +9,12 @@ class Square:
 
     @property
     def size(self):
+        """Initialise Square object with size."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Size of square and raise errors"""
         self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -20,6 +22,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """Calculates the area of a square"""
         area = self.__size * self.__size
         return area
 
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-# class Square that defines a square by: (based on 4-square.py)
+"""class Square that defines a square by: (based on 4-square.py)"""
 
 
 class Square:
+    """Initializes class square"""
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
+        """Initialise Square object with size."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Size of square and raise errors"""
         self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -19,10 +22,12 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """Calculates the area of a square"""
         area = self.__size * self.__size
         return area
 
     def my_print(self):
+        """Prints in square with the '#' character"""
         if self.__size is 0:
             print()
         else:
