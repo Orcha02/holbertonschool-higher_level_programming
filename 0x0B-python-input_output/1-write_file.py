b45cd26185adae3f81de6a0e1a996ee459c32c14
Orcha02 <orcha2002@gmail.com>
2021-06-02 23:56:19 +0000

    Add documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Function writes string to a text file, returns number of chars written"""
+
+
 def write_file(filename="", text=""):
+    """ Write file"""
     with open(filename, 'w') as open_file:
         return open_file.write(text)
