1888c3c9786d14500298d46db2e196b0650d608a
Orcha02 <orcha2002@gmail.com>
2021-06-02 03:23:12 +0000

    Add 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class MyInt"""
+
+
+class MyInt(int):
+    """Function Equal"""
+    def __ne__(self, other):
+        return int.__eq__(self, other)
+
+    """Function no equal"""
+    def __eq__(self, other):
+        return int.__ne__(self, other)
+
+
+
+# __ne__-> ==
+# __eq__-> !=
