eccf317057ec6fed19fda8fee63a143c716d9987
Orcha02 <orcha2002@gmail.com>
2021-06-02 23:53:07 +0000

    Add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
+    """Read file"""
     with open(filename, 'r') as open_file:
             print(open_file.read(), end="")
 
