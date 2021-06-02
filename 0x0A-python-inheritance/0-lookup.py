9bdedca04c46d8d60f096955d7e0e414a3d2308b
Orcha02 <orcha2002@gmail.com>
2021-06-02 00:54:40 +0000

    Reorganize comment

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-""" Returns the list of available attributes and methods of an object """
-
-
 def lookup(obj):
+    """ Returns the list of available attributes and methods of an object """
     return dir(obj)
 
 # dir-> Returns all attributes of the passed object
