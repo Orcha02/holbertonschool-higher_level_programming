3623c280bb16145a2ad30ec066354c66c3269909
Orcha02 <orcha2002@gmail.com>
2021-06-02 01:52:54 +0000

    check that type (obj) is not an instance of class a_class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,10 +7,12 @@ or indirectly) from the specified class, otherwise return False.
 
 def inherits_from(obj, a_class):
     """ Function for attributes """
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
 
 # issubclass-> checks if instance obj is a subclass of a_class
 # type(obj)-> since first param of issubclass must be a class, and type
 #             returns class type of the argument(object) passed as parameter
+# [type(obj) is not a_class]-> Checks that type (obj) is not
+#                               an instance of class a_class
