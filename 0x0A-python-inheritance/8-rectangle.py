ea0783a328ef8601a9e1e0c62a3130afaec84fe8
Orcha02 <orcha2002@gmail.com>
2021-06-02 02:05:17 +0000

    Reorganize code:

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,22 +2,7 @@
 """ Class BaseGeometry (based on 7-base_geometry.py). """
 
 
-class BaseGeometry:
-    """ BaseGeometry class"""
-    def area(self):
-        """ Function for attributes """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Public instance method that validates value"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-# Could just put instead of all the code above:
-# BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 """Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)"""
 
 
