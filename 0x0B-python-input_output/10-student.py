60a665fd41ea80fe8a726f5097c0d40a5436c87b
Orcha02 <orcha2002@gmail.com>
2021-06-03 00:25:20 +0000

    Add 10-student.py with documentation

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Class Student that defines a student by: (based on 9-student.py) """
+
+
+class Student:
+    """ Create class Student """
+    def __init__(self, first_name, last_name, age):
+        """ Initialize class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for attributes in attrs:
+            if attributes in self.__dict__:
+                dict[attributes] = self.__dict__[attributes]
+            return dict
