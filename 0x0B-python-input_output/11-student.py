d105f00db8cfdce5d2cd3cc5519fa2746764420f
Orcha02 <orcha2002@gmail.com>
2021-06-03 01:46:38 +0000

    Add 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Class Student that defines a student by: (based on 10-student.py) """
+
+
+class Student:
+    """ Create class Student """
+    def __init__(self, first_name, last_name, age):
+        """ Initialize class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for attributes in attrs:
+            if attributes in self.__dict__:
+                dict[attributes] = self.__dict__[attributes]
+        return dict
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for item in json:
+            self.__dict__[item] = json[item]
