8e9405e4a50fa78bdc38b3bc64e8c0b071c83fcb
Orcha02 <orcha2002@gmail.com>
2021-06-03 00:27:01 +0000

    Check pep8 and attrs in def

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,7 +10,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ Retrieves a dictionary representation of a Student instance """
         if attrs is None:
             return self.__dict__
