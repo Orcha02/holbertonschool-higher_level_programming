f54d0e63435c6fa39a3480f9555d17f104620fe7
Orcha02 <orcha2002@gmail.com>
2021-06-05 03:38:05 +0000

    Add Informal representation of Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,8 @@ class Rectangle(Base):
             print()
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Return string representation of class."""
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format
+        (self.id, self.__x, self.__y, self.__width, self.__height)
