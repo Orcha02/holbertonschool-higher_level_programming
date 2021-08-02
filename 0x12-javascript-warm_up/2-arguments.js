403c7f2dde31e3002b9b97358f7d35b76659a8bc
Orcha02 <orcha2002@gmail.com>
2021-08-02 20:00:05 +0000

    Add tasks 2-3

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv.length;
+
+if (args < 2) {
+  console.log('No argument');
+} else if (args === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+
+// Arg(0)-> Process execution path
+// Arg(1)-> Executable file
+// Arg(2)-> 1st Argument
