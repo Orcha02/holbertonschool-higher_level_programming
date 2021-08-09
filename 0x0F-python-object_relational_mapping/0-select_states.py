772fd3745852384d1baca90b4e877c1d7cf24c26
Orcha02 <orcha2002@gmail.com>
2021-08-09 00:01:44 +0000

    Re-organize comments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,14 +7,12 @@ if __name__ == "__main__":
     """  Open database connection """
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
-# Prepare a cursor object using cursor() method
-cursor = db.cursor()
-# Query the database - must be sorted in ascending order by states.id
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-# Fetch all rows
-all_rows = cursor.fetchall()
-for row in all_rows:
-    print(row)
-# Disconnect from server
-cursor.close()
-db.close()
+
+    cursor = db.cursor() # Prepare a cursor object using cursor() method
+    cursor.execute("SELECT * FROM states ORDER BY id ASC") # Query the database - must be sorted in ascending order by states.id
+
+    all_rows = cursor.fetchall() # Fetch all rows
+    for row in all_rows:
+        print(row)
+    cursor.close() # Disconnect from server
+    db.close()
