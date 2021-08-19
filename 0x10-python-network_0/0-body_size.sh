dcafa7ca768f1525dccb1288bc6c09d81eec8abe
Orcha02 <orcha2002@gmail.com>
2021-08-19 23:52:06 +0000

    Erase comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Take url, send request, display size of body (The size must be displayed in bytes)
 curl -s "$1" | wc -c
-
-# wc-> Word Count
-# -c-> Displays count of bytes present in file
