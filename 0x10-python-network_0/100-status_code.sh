ef7bdfd1b2507c07f0d62c31855bd749ab16d946
Orcha02 <orcha2002@gmail.com>
2021-08-20 03:14:02 +0000

    Erase comments

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
 curl -sI "$1" -w "%{http_code}" -o /dev/null
-
-# -w-> What to output after completion
-# "%{http_code}"-> Prints the HTTP_CODE to stdout
-# -o-> Write output to <file> instead of stdout
-# /dev/null-> Instead of reading, it’s used to write. Whatever you write to
-#             /dev/null will be discarded, forgotten into the void.
