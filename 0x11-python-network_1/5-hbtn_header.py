fbb667138a698565d369d771ba45b6b6f8e8e212
Orcha02 <orcha2002@gmail.com>
2021-08-20 20:37:11 +0000

    Add tasks 5-6

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL and displays the value of
+the variable X-Request-Id in the response header using the package requests
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
