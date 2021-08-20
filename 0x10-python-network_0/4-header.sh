c88e871e2eb08a41a8c555b5f9080b0cac1e70c4
Orcha02 <orcha2002@gmail.com>
2021-08-20 02:15:52 +0000

    Fix task 1 comment, add tasks 3-5

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes URL, sends GET request display body
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script that do a POST request and sent two values/variables
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
