dbea92ae1f69631205cd350755ce97fddb8a3052
Orcha02 <orcha2002@gmail.com>
2021-08-20 02:47:16 +0000

    Change built-in

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,11 @@
 
 def find_peak(list_of_integers):
     """ Peak in a list of unsorted integers """
-    if len(list_of_integers) == 0:
-        return None
-    return max(list_of_integers)
+    if type(list_of_integers) == list:
+        if len(list_of_integers) == 0:
+            return None
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    return None
+
+# [-1]-> Returns the last value in the resulting list
