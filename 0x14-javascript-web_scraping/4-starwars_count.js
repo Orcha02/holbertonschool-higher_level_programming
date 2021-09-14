496ab8661d3cc0addbe95e0238ad2227ec8ea238
Orcha02 <orcha2002@gmail.com>
2021-09-14 18:06:52 -0500

    Fix semistandard with cammel case variables

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const wedge_antilles = '18';
-let num_movies = 0;
+const wedgeAntilles = '18';
+let numMovies = 0;
 
 request(url, function (error, response, body) {
-    if (error) {
-	console.log(error);
-    } else {
-	const films = JSON.parse(body).results;
-	for (const movie_info in films) {
-            const characters = films[movie_info].characters;
-            for (const character_id in characters)
-		if (characters[character_id].includes(wedge_antilles)) {
-                    num_movies++;
-		}
-	}
-	console.log(num_movies);
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    for (const movieInfo in films) {
+      const characters = films[movieInfo].characters;
+      for (const characterId in characters) {
+        if (characters[characterId].includes(wedgeAntilles)) {
+          numMovies++;
+        }
+      }
     }
+    console.log(numMovies);
+  }
 });
