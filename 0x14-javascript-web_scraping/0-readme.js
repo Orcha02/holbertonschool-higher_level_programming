3162460abbca4f782021b7668af3b717a630a2f6
Orcha02 <orcha2002@gmail.com>
2021-09-14 14:21:28 -0500

    Fix boolean

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // Use fs.readFile() method to read file
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   // Display file content or error content
-  if (err === true) {
+  if (err) {
     console.log(err);
   }
   console.log(data);
