b7dbe574842dfb22e37d5b06f7b9ab9a88375eab
Orcha02 <orcha2002@gmail.com>
2021-09-22 21:55:16 -0500

    Add advanced tasks 100-102

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  $('#btn_translate').click(() => {
+    $.ajax({
+      url: 'https://www.fourtonfish.com/hellosalut/',
+      type: 'GET',
+      data: { lang: $('#language_code').val() },
+      success:(translate) => {
+        $('#hello').html(translate.hello);
+      }
+    });
+  });
+});
